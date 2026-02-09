6fcc87a45b4977fd9ca7ac08193bec02f0f263a1
Vishu <vishusaini687@gmail.com>
2026-02-09 15:18:22 +0000

    added selectionsort

---
+++ selectionsort.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
+
+using namespace std; 
+
+void selectionSort(vector <int>& arr, long long& passes, long long& comparisons, long long& shifts){
+    int n = arr.size();
+
+    for (int i = 0; i < n-1; i++){
+        passes++;
+        int smallestIdx = i;
+        for (int j = i + 1; j < n; j++){
+            comparisons++;
+            if (arr[j] < arr[smallestIdx]){
+                smallestIdx = j;
+            }
+        } //condition lagani h
+        swap(arr[i], arr[smallestIdx]);
+        shifts++;
+    }
+}
+
+
+int main(){
+    ifstream fin("arrays.csv");
+    ofstream fout("selection_results.csv");
+
+    if (!fin || !fout){
+        cout << "file error :/";
+        return 0;
+    }
+
+    fout << "Array_No,Type,Size,Passes,Comparisons,Shifts\n";   //headers add kiye hai
+
+    int arrayNum =  1;
+    int batchCount = 0;
+
+    long long sumPass = 0;
+    long long sumCom = 0;
+    long long sumShifts = 0;
+
+    while (true){
+        vector<int> arr;
+        char type; int size;
+        int number;
+        char comma;
+
+        if (!(fin >> type >> comma >> size >> comma)) break;
+
+        while (fin>>number){
+            arr.push_back(number);
+            if (fin.peek() == ','){
+                fin.ignore();
+            } else{
+                break;
+            }
+        }
+
+        char ch;
+        while (fin.get(ch) && ch != '\n');
+
+        long long passes = 0;
+        long long comparisons = 0;
+        long long shifts = 0;
+
+        selectionSort(arr, passes, comparisons, shifts);  //array sorted
+
+        fout << arrayNum << "," << type << "," << arr.size() << "," << 
+                passes << "," << comparisons << "," << shifts << endl;
+
+        sumPass += passes;
+        sumCom += comparisons;
+        sumShifts += shifts;
+        batchCount++;
+
+        if (batchCount == 10){
+            fout << "AVG,AVG,-,"<< 
+                    (double)sumPass/10 << "," <<
+                    (double)sumCom/10 << "," << (double)sumShifts/10 << "\n";
+
+            batchCount = 0;
+            sumPass =  0;
+            sumCom = 0;
+            sumShifts = 0;
+        }
+        arrayNum++;
+    }
+
+    fin.close();
+    fout.close();
+    cout<< "Selection sort completed :)";
+
+    return 0;
+
+}
